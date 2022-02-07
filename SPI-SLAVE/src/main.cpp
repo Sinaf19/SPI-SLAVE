914f49d17afff6b38b4be1a2f127517b90de5279
Sinaf19 <95037511+Sinaf19@users.noreply.github.com>
2022-02-07 10:40:46 +0100

    Chgmt

--- SPI-SLAVE/src/main.cpp
+++ SPI-SLAVE/src/main.cpp
@@ -1,11 +1,21 @@
 #include <Arduino.h>
+#include <SPI.h>
+
+
 
 
 //SLAVE code
+#define LED 26 
+
+
 
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(19200);
+  SPI.begin();
+
+
 }
 
 void loop() {
