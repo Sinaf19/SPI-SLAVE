eed5916e1c0cb23abd88ae5038dab418a15241e7
Sinaf19 <95037511+Sinaf19@users.noreply.github.com>
2022-02-11 11:12:55 +0100

    Chgmt sd card

--- SPI-MASTER/src/main.cpp
+++ SPI-MASTER/src/main.cpp
@@ -60,7 +60,7 @@ bool ecranFlagh = true;
 Sd2Card card;
 SdVolume volume;
 SdFile root;
-const int chipSelect = 10;  
+const int chipSelect = 10;
 
 Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, CS_PIN, DC_PIN, MOSI_PIN, SCLK_PIN, RST_PIN);
 // Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, CS_PIN, DC_PIN, RST_PIN);
@@ -79,113 +79,59 @@ void setup()
   pinMode(LEDSwitch1, OUTPUT);
   pinMode(LEDSwitch2, OUTPUT);
   pinMode(SS2, OUTPUT);
-  
-  /* digitalWrite(SS, HIGH);
-  Serial.print("\nInitializing SD card...");
-
-    while (!card.init(SPI_HALF_SPEED, chipSelect)) {
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("* is a card is inserted?");
-    Serial.println("* Is your wiring correct?");
-    Serial.println("* did you change the chipSelect pin to match your shield or module?");
-  } 
-  
-  // print the type of card
-  Serial.print("\nCard type: ");
-  switch(card.type()) {
-    case SD_CARD_TYPE_SD1:
-      Serial.println("SD1");
-      break;
-    case SD_CARD_TYPE_SD2:
-      Serial.println("SD2");
-      break;
-    case SD_CARD_TYPE_SDHC:
-      Serial.println("SDHC");
-      break;
-    default:
-      Serial.println("Unknown");
-  }
-        // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
-  if (!volume.init(card)) {
-    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
-    return;
-  }
 
 
-  // print the type and size of the first FAT-type volume
-  uint32_t volumesize;
-  Serial.print("\nVolume type is FAT");
-  Serial.println(volume.fatType(), DEC);
-  Serial.println();
-  
-  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
-  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
-  volumesize *= 512;                            // SD card blocks are always 512 bytes
-  Serial.print("Volume size (bytes): ");
-  Serial.println(volumesize);
-  Serial.print("Volume size (Kbytes): ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-  Serial.print("Volume size (Mbytes): ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-
-  
-  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
-  root.openRoot(volume);
-  
-  // list all files in the card with date and size
-  root.ls(LS_R | LS_DATE | LS_SIZE); */
-
-     pinMode(MISO, INPUT);
-    pinMode(SS, OUTPUT); 
+  pinMode(MOSI, OUTPUT);
+  pinMode(SS, OUTPUT);
 
   //  digitalWrite(SS, HIGH);
   //  digitalWrite(SS2, HIGH);
   // SPI.setClockDivider(SPI_CLOCK_DIV128);
 
-   tft.fillScreen(BLACK);
+  tft.fillScreen(BLACK);
   tft.setCursor(0, 50);
   tft.setTextColor(WHITE);
   tft.setTextSize(1);
   tft.println("Initialisation");
-  // tft.fillScreen(BLACK); 
-    digitalWrite(SS, LOW);
-
-
+  // tft.fillScreen(BLACK);
+  digitalWrite(SS, LOW);
 }
 
 void loop()
 {
-      SPI.beginTransaction(SPISettings(100000, MSBFIRST, SPI_MODE0));
-    // put your main code here, to run repeatedly:
-  Serial.println(digitalRead(SS));
+
+  buttonValue2 = digitalRead(button2);
+  buttonValue4 = digitalRead(button4);
+
+    
+    SPI.beginTransaction(SPISettings(400000, MSBFIRST, SPI_MODE0));
+   
+    digitalWrite(SS, LOW);
     buttonValue1 = digitalRead(button);
 
     buttonValue3 = digitalRead(button3);
-    buttonValue4 = digitalRead(button4);
+    
     /* int potentiometre = analogRead(pot);
     bool ss1 = digitalRead(SS);
     bool ss2 = digitalRead(SS2); */
 
-    
     if (buttonValue1)
     {
       x = 1;
-    } else {
+    }
+    else
+    {
 
       x = 0;
-
     }
 
-    
     Mastersend = x;
     Mastereceive = SPI.transfer(Mastersend);
-    Serial.print("Mastereceive : ");
+  /*Serial.print("Mastereceive : ");
     Serial.println(Mastereceive);
     Serial.print("Mastersend : ");
-    Serial.println(Mastersend);
-   
+    Serial.println(Mastersend);  */
+
     if (Mastereceive == 85)
     {
       digitalWrite(LED, HIGH);
@@ -194,145 +140,147 @@ void loop()
     {
       digitalWrite(LED, LOW);
     }
-
+    
     SPI.endTransaction();
+    delay(10);
+    
+    
+  
 }
 
-
-
-void buttonFunction(){
+void buttonFunction()
+{
   buttonValue2 = digitalRead(button2);
-  
-    digitalWrite(SS2, HIGH);
-    SPI.beginTransaction(SPISettings(9600, MSBFIRST, SPI_MODE0));
-    // put your main code here, to run repeatedly:
 
-    buttonValue1 = digitalRead(button);
+  digitalWrite(SS2, HIGH);
+  SPI.beginTransaction(SPISettings(9600, MSBFIRST, SPI_MODE0));
+  // put your main code here, to run repeatedly:
 
-    buttonValue3 = digitalRead(button3);
-    buttonValue4 = digitalRead(button4);
-    /* int potentiometre = analogRead(pot);
-    bool ss1 = digitalRead(SS);
-    bool ss2 = digitalRead(SS2); */
+  buttonValue1 = digitalRead(button);
 
-    if (buttonValue1 == HIGH and buttonValue2 == HIGH)
-    {
-      x = 0;
-      digitalWrite(LEDSwitch1, LOW);
-      digitalWrite(LEDSwitch2, LOW);
-    }
-    else if (buttonValue1)
-    {
-      x = 1;
-    }
-    else if (buttonValue3)
-    {
-      x = 3;
-      digitalWrite(LEDSwitch2, HIGH);
-    }
-    else
-    {
-      x = 0;
-      digitalWrite(LEDSwitch1, LOW);
-      digitalWrite(LEDSwitch2, LOW);
-    }
+  buttonValue3 = digitalRead(button3);
+  buttonValue4 = digitalRead(button4);
+  /* int potentiometre = analogRead(pot);
+  bool ss1 = digitalRead(SS);
+  bool ss2 = digitalRead(SS2); */
 
-    if (buttonValue4 != buttonValue4_Mem)
-    {
+  if (buttonValue1 == HIGH and buttonValue2 == HIGH)
+  {
+    x = 0;
+    digitalWrite(LEDSwitch1, LOW);
+    digitalWrite(LEDSwitch2, LOW);
+  }
+  else if (buttonValue1)
+  {
+    x = 1;
+  }
+  else if (buttonValue3)
+  {
+    x = 3;
+    digitalWrite(LEDSwitch2, HIGH);
+  }
+  else
+  {
+    x = 0;
+    digitalWrite(LEDSwitch1, LOW);
+    digitalWrite(LEDSwitch2, LOW);
+  }
 
-      buttonValue4_Mem = buttonValue4;
-      if (buttonValue4 == 1)
-      {
-        chgmtEcran = 1;
-        ecranFlag = true;
-        Serial.println("Temperature");
-      }
-    }
-
-    if (buttonValue4 == 1)
-    {
-      x = 2;
-    }
-
-    if (buttonValue3 != buttonValue3_Mem)
-    {
-
-      buttonValue3_Mem = buttonValue3;
-      if (buttonValue3 == 1)
-      {
-        chgmtEcranh = 1;
-        ecranFlagh = true;
-      }
-    }
-
-    digitalWrite(SS, LOW);
-    Mastersend = x;
-    Mastereceive = SPI.transfer(Mastersend);
-    Serial.print("Mastereceive : ");
-    Serial.println(Mastereceive);
-    Serial.print("Mastersend : ");
-    Serial.println(Mastersend);
-
-    if (chgmtEcran == 1 and ecranFlag == true and Mastereceive >= 10)
-    {
-      tft.fillScreen(RED);
-      tft.setCursor(5, 55);
-      tft.setTextSize(1);
-      tft.setTextColor(WHITE);
-      tft.print("Temperature : ");
-      Serial.println("Temperature");
-      tft.println(Mastereceive);
-      ecranFlag = false;
-    }
-    else if (chgmtEcran == 0 and ecranFlag == true)
-    {
-      tft.fillScreen(BLACK);
-      ecranFlag = false;
-    }
-
-    if (chgmtEcranh == 1 and ecranFlagh == true and Mastereceive >= 5)
-    {
-      tft.fillScreen(BLUE);
-      tft.setCursor(5, 55);
-      tft.setTextSize(1);
-      tft.setTextColor(WHITE);
-      tft.print("Humidity : ");
-      Serial.println("Humidity");
-      tft.println(Mastereceive);
-      ecranFlagh = false;
-    }
-    else if (chgmtEcranh == 0 and ecranFlagh == true)
-    {
-      tft.fillScreen(BLACK);
-      ecranFlagh = false;
-    }
-
-    if (Mastereceive == 85)
-    {
-      digitalWrite(LED, HIGH);
-    }
-    else
-    {
-      digitalWrite(LED, LOW);
-    }
-
-    SPI.endTransaction();
-  
-
-/*   if (buttonValue2 == 0)
+  if (buttonValue4 != buttonValue4_Mem)
   {
 
-    digitalWrite(SS, HIGH);
-    digitalWrite(SS2, LOW);
-    Serial.println("In the matrix");
-    SPI.beginTransaction(SPISettings(9600, MSBFIRST, SPI_MODE0));
+    buttonValue4_Mem = buttonValue4;
+    if (buttonValue4 == 1)
+    {
+      chgmtEcran = 1;
+      ecranFlag = true;
+      Serial.println("Temperature");
+    }
+  }
 
-    Mastersend = 30;
+  if (buttonValue4 == 1)
+  {
+    x = 2;
+  }
 
-    Mastersend = SPI.transfer(Mastereceive);
-    Serial.println(Mastereceive);
-    analogWrite(BlueLED, Mastereceive);
+  if (buttonValue3 != buttonValue3_Mem)
+  {
 
-    SPI.endTransaction();
-  } */
+    buttonValue3_Mem = buttonValue3;
+    if (buttonValue3 == 1)
+    {
+      chgmtEcranh = 1;
+      ecranFlagh = true;
+    }
+  }
+
+  digitalWrite(SS, LOW);
+  Mastersend = x;
+  Mastereceive = SPI.transfer(Mastersend);
+/*   Serial.print("Mastereceive : ");
+  Serial.println(Mastereceive);
+  Serial.print("Mastersend : ");
+  Serial.println(Mastersend);
+ */
+  if (chgmtEcran == 1 and ecranFlag == true and Mastereceive >= 10)
+  {
+    tft.fillScreen(RED);
+    tft.setCursor(5, 55);
+    tft.setTextSize(1);
+    tft.setTextColor(WHITE);
+    tft.print("Temperature : ");
+    //Serial.println("Temperature");
+    tft.println(Mastereceive);
+    ecranFlag = false;
+  }
+  else if (chgmtEcran == 0 and ecranFlag == true)
+  {
+    tft.fillScreen(BLACK);
+    ecranFlag = false;
+  }
+
+  if (chgmtEcranh == 1 and ecranFlagh == true and Mastereceive >= 5)
+  {
+    tft.fillScreen(BLUE);
+    tft.setCursor(5, 55);
+    tft.setTextSize(1);
+    tft.setTextColor(WHITE);
+    tft.print("Humidity : ");
+  //  Serial.println("Humidity");
+    tft.println(Mastereceive);
+    ecranFlagh = false;
+  }
+  else if (chgmtEcranh == 0 and ecranFlagh == true)
+  {
+    tft.fillScreen(BLACK);
+    ecranFlagh = false;
+  }
+
+  if (Mastereceive == 85)
+  {
+    digitalWrite(LED, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED, LOW);
+  }
+
+  SPI.endTransaction();
+
+  /*   if (buttonValue2 == 0)
+    {
+
+      digitalWrite(SS, HIGH);
+      digitalWrite(SS2, LOW);
+      Serial.println("In the matrix");
+      SPI.beginTransaction(SPISettings(9600, MSBFIRST, SPI_MODE0));
+
+      Mastersend = 30;
+
+      Mastersend = SPI.transfer(Mastereceive);
+      Serial.println(Mastereceive);
+      analogWrite(BlueLED, Mastereceive);
+
+      SPI.endTransaction();
+    } */
 }
---
+++ SPI-SDCARD/src/main.cpp
@@ -0,0 +1,69 @@
+
+
+// Include des librairies
+#include <Arduino.h>
+#include <SD.h>
+#include <SPI.h>
+
+File myFile;
+
+// change this to match your SD shield or module;
+//     Arduino Ethernet shield: pin 4
+//     Adafruit SD shields and modules: pin 10
+//     Sparkfun SD shield: pin 8
+const int chipSelect = 53;
+
+void setup()
+{
+ // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  Serial.print("Initializing SD card...");
+  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
+  // Note that even if it's not used as the CS pin, the hardware SS pin 
+  // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
+  // or the SD library functions will not work. 
+   pinMode(SS, OUTPUT);
+   pinMode(SS, LOW);
+  if (!SD.begin(chipSelect)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+  
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open("test.txt", FILE_WRITE);
+  
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to test.txt...");
+    myFile.println("Hello, it's me, I've been wondering if after all these years you'd like to meet. ");
+	// close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+  
+  // re-open the file for reading:
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("test.txt:");
+    
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+    	Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+  	// if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
+
+void loop()
+{
+	// nothing happens after setup
+}
