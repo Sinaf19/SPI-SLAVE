010c5c0a970db2a67f10bdc8848411f4f5154cb5
Sinaf19 <95037511+Sinaf19@users.noreply.github.com>
2022-03-08 07:27:24 +0100

    séodhf

--- SPI-SDCARD/src/main.cpp
+++ SPI-SDCARD/src/main.cpp
@@ -1,58 +1,136 @@
-
-
-// Include des librairies
 #include <Arduino.h>
-#include <SD.h>
 #include <SPI.h>
 
-File myFile;
+// Pins
+#define ENCA 2
+#define ENCB 3
+#define PWM 8
+#define IN1 26
+#define IN2 27
 
-// change this to match your SD shield or module;
-//     Arduino Ethernet shield: pin 4
-//     Adafruit SD shields and modules: pin 10
-//     Sparkfun SD shield: pin 8
-const int chipSelect = 22;
+// globals
+long prevT = 0;
+int posPrev = 0;
+void readEncoder();
+void setMotor(int dir, int pwmVal, int pwm, int in1, int in2);
+// Use the "volatile" directive for variables
+// used in an interrupt
+volatile int pos_i = 0;
+volatile float velocity_i = 0;
+volatile long prevT_i = 0;
 
-void setup()
-{
- // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  Serial.print("Initializing SD card...");
-  pinMode(SS, OUTPUT);
-  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
-  // Note that even if it's not used as the CS pin, the hardware SS pin 
-  // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
-  // or the SD library functions will not work. 
-  pinMode(chipSelect, OUTPUT);
-  digitalWrite(chipSelect, LOW);
-  if (!SD.begin(chipSelect)) {
-    Serial.println("initialization failed!");
-    return;
+float v1Filt = 0;
+float v1Prev = 0;
+float v2Filt = 0;
+float v2Prev = 0;
+
+float eintegral = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(ENCA,INPUT);
+  pinMode(ENCB,INPUT);
+  pinMode(PWM,OUTPUT);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(ENCA),
+                  readEncoder,RISING);
+}
+
+void loop() {
+
+  // read the position and velocity
+  int pos = 0;
+  float velocity2 = 0;
+  noInterrupts(); // disable interrupts temporarily while reading
+  pos = pos_i;
+  velocity2 = velocity_i;
+  interrupts(); // turn interrupts back on
+
+  // Compute velocity with method 1
+  long currT = micros();
+  float deltaT = ((float) (currT-prevT))/1.0e6;
+  float velocity1 = (pos - posPrev)/deltaT;
+  posPrev = pos;
+  prevT = currT;
+
+  // Convert count/s to RPM
+  float v1 = velocity1/600.0*60.0;
+  float v2 = velocity2/600.0*60.0;
+
+  // Low-pass filter (25 Hz cutoff)
+  v1Filt = 0.854*v1Filt + 0.0728*v1 + 0.0728*v1Prev;
+  v1Prev = v1;
+  v2Filt = 0.854*v2Filt + 0.0728*v2 + 0.0728*v2Prev;
+  v2Prev = v2;
+
+  // Set a target
+  float vt = 100*(sin(3/1e6)>0);
+
+  // Compute the control signal u
+  float kp = 5;
+  float ki = 10;
+  float e = vt-v1Filt;
+  eintegral = eintegral + e*deltaT;
+  
+  float u = kp*e + ki*eintegral;
+
+  // Set the motor speed and direction
+  int dir = 1;
+  if (u<0){
+    dir = -1;
   }
-  Serial.println("initialization done.");
-  
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  myFile = SD.open("test.txt", FILE_WRITE);
-  
-  // re-open the file for reading:
-  myFile = SD.open("test.txt");
-  if (myFile) {
-    Serial.println("test.txt:");
-    
-    // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-    	Serial.write(myFile.read());
-    }
-    // close the file:
-    myFile.close();
-  } else {
-  	// if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
+  int pwr = (int) fabs(u);
+  if(pwr > 255){
+    pwr = 255;
+  }
+  setMotor(dir,pwr,PWM,IN1,IN2);
+
+  Serial.print(vt);
+  Serial.print(" ");
+  Serial.print(v1Filt);
+  Serial.println();
+  delay(1);
+}
+
+void setMotor(int dir, int pwmVal, int pwm, int in1, int in2){
+  analogWrite(pwm,pwmVal); // Motor speed
+  if(dir == 1){ 
+    // Turn one way
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+  }
+  else if(dir == -1){
+    // Turn the other way
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,HIGH);
+  }
+  else{
+    // Or dont turn
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,LOW);    
   }
 }
 
-void loop()
-{
-	// nothing happens after setup
+void readEncoder(){
+  // Read encoder B when ENCA rises
+  int b = digitalRead(ENCB);
+  int increment = 0;
+  if(b>0){
+    // If B is high, increment forward
+    increment = 1;
+  }
+  else{
+    // Otherwise, increment backward
+    increment = -1;
+  }
+  pos_i = pos_i + increment;
+
+  // Compute velocity with method 2
+  long currT = micros();
+  float deltaT = ((float) (currT - prevT_i))/1.0e6;
+  velocity_i = increment/deltaT;
+  prevT_i = currT;
 }
